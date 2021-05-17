d8d780cff670a9ced271fe6d6d8455e84b20a694
Chester Cabahug <chestercabahug26@gmail.com>
2021-05-17 08:12:40 +0800

    changed the date tto June 6th

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ function Clock(countdown,callback) {
   setTimeout(updateClock,500);
 }
 
-var deadline = new Date("May 24, 2021 0:0:00");
+var deadline = new Date("June 6, 2021 0:0:00");
 var c = new Clock(deadline, function(){ console.log('countdown complete') });
 document.body.appendChild(c.el);
 
