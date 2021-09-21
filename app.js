2e417a95493f3fdef763dea7b4d03f72a2c8e942
Chester Cabahug <chestercabahug26@gmail.com>
2021-09-21 09:50:17 +0800

    test

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ function Clock(countdown,callback) {
   setTimeout(updateClock,500);
 }
 
-var deadline = new Date("September 6, 2021 0:0:00");
+var deadline = new Date("September 30, 2021 0:0:00");
 var c = new Clock(deadline, function(){ console.log('countdown complete') });
 document.body.appendChild(c.el);
 
